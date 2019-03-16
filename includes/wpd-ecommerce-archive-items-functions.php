46535d56b85146ae6284a2009bd25ac3b3edc31d
Robert DeVore <deviodigital@gmail.com>
2019-03-16 14:51:07 -0400

    :package: NEW: get_wpd_ecommerce_product_buttons helper function

--- includes/wpd-ecommerce-archive-items-functions.php
+++ includes/wpd-ecommerce-archive-items-functions.php
@@ -61,3 +61,52 @@ add_action( 'wpd_topicals_widget_inside_bottom', 'wpd_ecommerce_archive_items_bu
 add_action( 'wpd_growers_widget_inside_bottom', 'wpd_ecommerce_archive_items_buttons' );
 add_action( 'wpd_gear_widget_inside_bottom', 'wpd_ecommerce_archive_items_buttons' );
 add_action( 'wpd_tinctures_widget_inside_bottom', 'wpd_ecommerce_archive_items_buttons' );
+
+/**
+ * Product Buttons
+ * 
+ * This function gets the buy buttons for products
+ * 
+ * @since 1.0
+ */
+function get_wpd_ecommerce_product_buttons( $product_id ) {
+
+    $post_type      = get_post_type( $product_id );
+    $price_each     = get_post_meta( $product_id, '_priceeach', true );
+    $price_topical  = get_post_meta( $product_id, '_pricetopical', true );
+    $price_per_pack = get_post_meta( $product_id, '_priceperpack', true );
+
+    $str = '';
+
+    if ( 'edibles' == $post_type || 'prerolls' == $post_type || 'growers' == $post_type || 'tinctures' == $post_type || 'gear' == $post_type ) {
+    	if ( '' != $price_each && '' != $price_per_pack ) {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '" class="button wpd-buy-btn">' . __( 'Select Options', 'wpd-ecommerce' ) . '</a>';
+        } elseif ( '' === $price_each && '' != $price_per_pack ) {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '?add_item=' . $product_id . '_priceperpack" class="button wpd-buy-btn">' . __( 'Buy Now', 'wpd-ecommerce' ) . '</a>';
+        } elseif ( '' != $price_each && '' === $price_per_pack ) {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '?add_item=' . $product_id . '_priceeach" class="button wpd-buy-btn">' . __( 'Buy Now', 'wpd-ecommerce' ) . '</a>';
+        } else {
+            // Do nothing.
+        }
+    }
+    if ( 'topicals' == $post_type ) {
+    	if ( '' != $price_topical && '' != $price_per_pack ) {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '" class="button wpd-buy-btn">' . __( 'Select Options', 'wpd-ecommerce' ) . '</a>';
+        } elseif ( '' === $price_topical && '' != $price_per_pack ) {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '?add_item=' . $product_id . '_priceperpack" class="button wpd-buy-btn">' . __( 'Buy Now', 'wpd-ecommerce' ) . '</a>';
+        } elseif ( '' != $price_topical && '' === $price_per_pack ) {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '?add_item=' . $product_id . '_pricetopical" class="button wpd-buy-btn">' . __( 'Buy Now', 'wpd-ecommerce' ) . '</a>';
+        } else {
+            // Do nothing.
+        }
+    }
+    if ( 'flowers' == $post_type || 'concentrates' == $post_type ) {
+        if ( '' != $price_each ) {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '?add_item=' . $product_id . '_priceeach" class="button wpd-buy-btn">' . __( 'Buy Now', 'wpd-ecommerce' ) . '</a>';
+        } else {
+            $str .= '<a href="' . get_the_permalink( $product_id ) . '" class="button wpd-buy-btn">' . __( 'Select Options', 'wpd-ecommerce' ) . '</a>';
+        }
+    }
+
+    return $str;
+}
