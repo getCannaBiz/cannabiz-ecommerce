dd13d069673eb12f6e2262812ea4cd501487644d
Robert DeVore <deviodigital@gmail.com>
2019-04-15 15:28:05 -0400

    :ok_hand: IMPROVE: Updated order archive redirects to use the Pages selected in WPD Settings

--- templates/archive-orders.php
+++ templates/archive-orders.php
@@ -6,20 +6,31 @@ get_header(); ?>
 <section id="primary" class="site-content">
     <div id="content" role="main">
         <?php
+            // Get pages data from settings page.
+            $wpdas_pages = get_option( 'wpdas_pages' );
+
+            // Get menu page selected by website owner.
+            $account_page = $wpdas_pages['wpd_pages_setup_account_page'];
+
+            // Get menu page selected by website owner.
+            $menu_page = $wpdas_pages['wpd_pages_setup_menu_page'];
+
+            // Redirect visitors.
             if ( is_user_logged_in() ) {
-                $user              = wp_get_current_user();
-                $role              = ( array ) $user->roles;
-                $order_customer_id = get_post_meta( get_the_ID(), 'wpd_order_customer_id', true );
+
+                // Get user info.
+                $user = wp_get_current_user();
+                $role = ( array ) $user->roles;
 
                 // Redirect patient.
                 if ( 'patient' === $role[0] ) {
-                    wp_redirect( home_url() . '/account/' );
+                    wp_redirect( $account_page );
                 } else {
-                    wp_redirect( home_url() . '/dispensary-menu/' );
+                    wp_redirect( $menu_page );
                 }
             } else {
                 // Redirect non-logged in users to menu page.
-                wp_redirect( home_url() . '/dispensary-menu/' );
+                wp_redirect( $menu_page );
             }
         ?>
     </div>
