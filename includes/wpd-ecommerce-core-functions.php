e335798c451c9380bc9b59a7af71a22821c9d952
Robert DeVore <deviodigital@gmail.com>
2021-09-08 21:20:39 -0400

    :ok_hand: IMPROVE: Updated growers inventory check

--- includes/wpd-ecommerce-core-functions.php
+++ includes/wpd-ecommerce-core-functions.php
@@ -1173,11 +1173,9 @@ function wpd_ecommerce_add_to_cart_form() {
     // Growers out of stock.
     if ( 'products' == get_post_type( get_the_ID() ) && 'growers' === get_post_meta( get_the_ID(), 'product_type', true ) ) {
         // If no clone or seed count has been added.
-        if ( ! get_post_meta( get_the_ID(), 'seed_count', true ) && ! get_post_meta( get_the_ID(), 'clone_count', true ) ) {
-            if ( ! get_post_meta( get_the_ID(), 'inventory_clones', true ) && ! get_post_meta( get_the_ID(), 'inventory_seeds', true ) ) {
-				$out_of_stock .= '<span class="wpd-inventory warning">' . __( 'out of stock', 'wpd-ecommerce' ) . '</span>';
-			}
-        }
+		if ( ! get_post_meta( get_the_ID(), 'inventory_clones', true ) && ! get_post_meta( get_the_ID(), 'inventory_seeds', true ) ) {
+			$out_of_stock .= '<span class="wpd-inventory warning">' . __( 'out of stock', 'wpd-ecommerce' ) . '</span>';
+		}
         // Add out of stock notice to output string.
         if ( get_post_meta( get_the_ID(), 'clone_count', true ) ) {
             if ( ! get_post_meta( get_the_ID(), 'inventory_clones', true ) ) {
