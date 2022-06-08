9ff5059db45db3a6eb91d79ba03c4602fbdfadd9
Robert DeVore <deviodigital@gmail.com>
2022-06-07 22:52:34 -0400

    :package: NEW: Added filters to include Heavyweight pricing in WPD arrays

--- includes/extend/heavyweights/admin/wpd-heavyweights-functions.php
+++ includes/extend/heavyweights/admin/wpd-heavyweights-functions.php
@@ -323,3 +323,71 @@ function wpd_heavyweights_concentrates_weights_high() {
     return $pricing_high;
 }
 add_filter( 'wpd_concentrates_pricing_high', 'wpd_heavyweights_concentrates_weights_high' );
+
+/**
+ * Add flower product prices
+ * 
+ * @param array $product_prices 
+ * 
+ * @since  2.2
+ * @return string
+ */
+function wpd_heavyweights_flowers_product_prices( $product_prices ) {
+    $product_prices['price_two_ounces']         = esc_attr__( '2 oz', 'wpd-ecommerce' );
+    $product_prices['price_quarter_pound']      = esc_attr__( '1/4 lb', 'wpd-ecommerce' );
+    $product_prices['price_half_pound']         = esc_attr__( '1/2 lb', 'wpd-ecommerce' );
+    $product_prices['price_one_pound']          = esc_attr__( '1 lb', 'wpd-ecommerce' );
+    $product_prices['price_two_pounds']         = esc_attr__( '2 lb', 'wpd-ecommerce' );
+    $product_prices['price_three_pounds']       = esc_attr__( '3 lb', 'wpd-ecommerce' );
+    $product_prices['price_four_pounds']        = esc_attr__( '4 lb', 'wpd-ecommerce' );
+    $product_prices['price_five_pounds']        = esc_attr__( '5 lb', 'wpd-ecommerce' );
+    $product_prices['price_six_pounds']         = esc_attr__( '6 lb', 'wpd-ecommerce' );
+    $product_prices['price_seven_pounds']       = esc_attr__( '7 lb', 'wpd-ecommerce' );
+    $product_prices['price_eight_pounds']       = esc_attr__( '8 lb', 'wpd-ecommerce' );
+    $product_prices['price_nine_pounds']        = esc_attr__( '9 lb', 'wpd-ecommerce' );
+    $product_prices['price_ten_pounds']         = esc_attr__( '10 lb', 'wpd-ecommerce' );
+    $product_prices['price_eleven_pounds']      = esc_attr__( '11 lb', 'wpd-ecommerce' );
+    $product_prices['price_twelve_pounds']      = esc_attr__( '12 lb', 'wpd-ecommerce' );
+    $product_prices['price_thirteen_pounds']    = esc_attr__( '13 lb', 'wpd-ecommerce' );
+    $product_prices['price_fourteen_pounds']    = esc_attr__( '14 lb', 'wpd-ecommerce' );
+    $product_prices['price_fifteen_pounds']     = esc_attr__( '15 lb', 'wpd-ecommerce' );
+    $product_prices['price_twenty_pounds']      = esc_attr__( '20 lb', 'wpd-ecommerce' );
+    $product_prices['price_twenty_five_pounds'] = esc_attr__( '25 lb', 'wpd-ecommerce' );
+    $product_prices['price_fifty_pounds']       = esc_attr__( '50 lb', 'wpd-ecommerce' );
+    return $product_prices;
+}
+add_filter( 'wpd_flowers_product_prices', 'wpd_heavyweights_flowers_product_prices' );
+
+/**
+ * Add product keys
+ * 
+ * @param array $price_keys 
+ * 
+ * @since  2.2
+ * @return array
+ */
+function wpd_heavyweights_prices_metabox_save_price_keys( $price_keys ) {
+    $price_keys[] = 'price_two_ounces';
+    $price_keys[] = 'price_quarter_pound';
+    $price_keys[] = 'price_half_pound';
+    $price_keys[] = 'price_one_pound';
+    $price_keys[] = 'price_two_pounds';
+    $price_keys[] = 'price_three_pounds';
+    $price_keys[] = 'price_four_pounds';
+    $price_keys[] = 'price_five_pounds';
+    $price_keys[] = 'price_six_pounds';
+    $price_keys[] = 'price_seven_pounds';
+    $price_keys[] = 'price_eight_pounds';
+    $price_keys[] = 'price_nine_pounds';
+    $price_keys[] = 'price_ten_pounds';
+    $price_keys[] = 'price_eleven_pounds';
+    $price_keys[] = 'price_twelve_pounds';
+    $price_keys[] = 'price_thirteen_pounds';
+    $price_keys[] = 'price_fourteen_pounds';
+    $price_keys[] = 'price_fifteen_pounds';
+    $price_keys[] = 'price_twenty_pounds';
+    $price_keys[] = 'price_twenty_five_pounds';
+    $price_keys[] = 'price_fifty_pounds';
+    return $price_keys;
+}
+add_filter( 'wp_dispensary_prices_metabox_save_price_keys', 'wpd_heavyweights_prices_metabox_save_price_keys' );
